c7fd800576ea892574c3b700f5bad72d2cb96bd1
Mutti <mustafa.atak@boun.edu.tr>
2023-02-23 12:13:30 +0300

    start added to json

--- app.js
+++ app.js
@@ -159,8 +159,7 @@ app.use((err,req,res,next) =>{
 });
 
 
-
-app.listen('3000', ()=>{
-    console.log("Server Started on 3000");
-
-});
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`)
+})
